a0797ce5363edf573d1ca9f92644cb88df59d4ac
bena3 <bena30757@gmail.com>
2024-11-11 10:43:49 +0900

    GPIO記入

--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
-from gpiozero import LED, Button  # gpiozeroモジュールを使用
+from gpiozero import Motor  # gpiozeroモジュールを使用
 import time
 
+
+hane_motor = Motor(23, 24) # ハネの制御
+sorenoido_motor = Motor(16, 20) # 射出用ソレノイド
+meta_motordriver = Motor(19, 26) # モータードライバーを操作するモータードライバー
+meta_motordriver.forward(1)
+
+def main():
+    pass
